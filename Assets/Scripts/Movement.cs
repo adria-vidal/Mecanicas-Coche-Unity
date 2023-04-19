ebc326764de6e0d9e50e4fdd244d33a180c1f4a9
Adria Vidal <adriavidal1998@gmail.com>
2023-04-19 20:12:23 +0200

    first commit y declaración de variable para movimiento

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    public float acceleration = 500f;
+    public float breakingForce = 300f;
+    private float currentAceleration = 0f;
+    private float currentBreakingForce = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
